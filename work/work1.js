2c6fcdf6b96e844f394a950a5442005391b8fd17
zhouyizhao <zhouyizhao8888@qq.com>
2019-10-08 17:36:51 +0800

    zyz

---
+++ work/work1.js
@@ -0,0 +1,55 @@
+const src=process.argv[2];
+if(src=="list"){
+  var fs=require('fs')
+  function geFileList(path){
+    var filesList=[];
+    readFile(path,filesList);
+    return filesList;
+  }
+  function readFile(path,fileList){
+    files=fs.readdirSync(path);
+    files.forEach(walk);
+    function walk(file){
+      states=fs.statSync(ptah+'/'+file);
+      if(states.isDirectory()){
+        readFile(path+'/'+file,filesList);
+      }
+      else{
+        var obj=new Object();
+        obj.size=states.size;
+        obj.name=file;
+        filesList.push(obj);
+      }
+    }
+  }
+  function writeFile(fileName,data){
+    fs.writeFile(filename,data,'utf-8',complete);
+    function complete(){
+      console.log("文件生成成功!");
+    }
+  }
+  var path = require("path");
+  var url = path.resolve('./');
+  var filesList=geFileList(url);
+  filesList.sort(sortHandler);
+  function sortHandler(a,b){
+    if(a.size>b.size)
+      return -1;
+    else if(a.size<b.size) return 1
+    return 0;
+  }
+  var str='';
+  for (var i=0;i<filesList.length;i++){
+    var item=fileList[i];
+    var desc=""+item.name+""+(item.size/1024).toFixed(2)+"";
+    str+=desc+"\n"
+  }
+  writeFile("text.txt",str);
+}
+else if(src=="mkdir folder"){
+  const fs=require('fs');
+  fs.writeFileSync(folder,'');
+}
+else{
+  console.log('命令行参数错误！')；
+}
